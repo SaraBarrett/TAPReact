6ec45ac8ca6a8133e6afeb22987c5b867947cdec
SaraBarrett <sara.c.mon@gmail.com>
2025-10-09 11:39:42 +0100

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let mySubject = "React";
+
+const subjects = ['JS', 'CSS', 'React', 'Bases de dados'];
+//vou à base de dados e verifico que o módulo é SQL
+
+function getRandomForSubject(max) {
+  return Math.floor(Math.random() * max);
+}
+
+mySubject = subjects[getRandomForSubject(4)];
+
+let product = {
+  name: 'caneta',
+  price: 5,
+  color: 'yellow'
+};
+
+function FirstComponent(){
+  return(
+  <header>
+    <h5>Aqui tenho o meu primeiro componente!</h5>
+  </header>
+  )
+}
+
+function MainGoal(){
+  return (
+    <div>
+        <p>O meu objectivo é aprender {mySubject} e construir aplicações incríveis.</p>
+        <p>O objecto é {product.name} e o preço é {product.price} euros</p>
+    </div>
+)
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <FirstComponent/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,6 +51,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <MainGoal/>
       <h1>Front End Developer: React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -25,6 +61,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
